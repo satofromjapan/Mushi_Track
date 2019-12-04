ae308dd40e762ba6a6de7f4718a8402e03f73805
Masato Takahashi <masatotakahashi@Masatos-MacBook-Pro.local>
2019-12-03 20:41:45 -0800

    initial commit

---
+++ mushi_track/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for mushi_track project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mushi_track.settings')
+
+application = get_wsgi_application()
